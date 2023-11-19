72b963b353c3cd2a58a93d45ef86f9c861880be8
Simon Nordlund <simon_nordlund@hotmail.com>
2023-11-19 14:08:11 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/storeQuestion", new StoreQuestionHandler());
+        server.start();
+    }
+
+    static class StoreQuestionHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String question = exchange.getRequestURI().getQuery();
+            question = question.substring(question.indexOf("=") + 1);
+
+            Files.write(Paths.get("src/question.txt"), (question + "\n").getBytes(), StandardOpenOption.APPEND);
+
+            String response = "Question stored successfully!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
